fb23fabb601ec6af2f8b99ee2a08969699d2047d
rameez-iqbal <rame14491@gmail.com>
2023-10-16 11:13:50 +0500

    svg icon set

--- components/Footer.vue
+++ components/Footer.vue
@@ -12,8 +12,8 @@
                     <div class="flex items-center">
                         <div class="flex items-center gap-3">
                             <div class="flex items-center justify-center bg-ftIcon h-12 w-12 rounded-full">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                    stroke="#ffffff" class="w-8 h-8">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" viewBox="0 0 24 24" stroke-width="1.5"
+                                    stroke="#AA90E1" class="w-8 h-8">
                                     <path stroke-linecap="round" stroke-linejoin="round"
                                         d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
                                 </svg>
